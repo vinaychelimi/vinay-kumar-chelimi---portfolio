48bcdd5f1a2e1d5e0d2a59ddffbdfe2a78cc8193
CHELIMI VINAY KUMAR <chelimivinaykumar.15@gmail.com>
2025-11-22 16:26:25 +0530

    Update profile image in About component

--- components/About.tsx
+++ components/About.tsx
@@ -30,7 +30,7 @@ export const About: React.FC = () => {
         variants={containerVariants}
       >
         <motion.div className="w-48 h-48 md:w-64 md:h-64 rounded-full overflow-hidden border-4 border-purple-500 neon-glow-purple flex-shrink-0" variants={itemVariants}>
-          <img src="https://media.licdn.com/dms/image/v2/D5603AQHEMOKIhPlgmA/profile-displayphoto-scale_400_400/B56Zqb6cHUIcAg-/0/1763552381235?e=1765411200&v=beta&t=ue0XVkkpC0_RA0etVSil8yu01fcPknj8riOm5S_8BZY" alt="Vinay Kumar Chelimi" className="w-full h-full object-cover"/>
+          <img src="https://scontent.fblr23-1.fna.fbcdn.net/v/t39.30808-1/580807992_1551783059588680_8479079005409076210_n.jpg?stp=c0.160.864.864a_dst-jpg_s200x200_tt6&_nc_cat=101&ccb=1-7&_nc_sid=e99d92&_nc_ohc=aGFPgY1kbgUQ7kNvwFo6jtj&_nc_oc=AdkSV69gfoaFWdTwN_AS49BZluFnrkJGw_3DkQhfQgjjrylVhAuPSvE-IP593DNkkf8&_nc_zt=24&_nc_ht=scontent.fblr23-1.fna&_nc_gid=lZ5iiTlJLLWnPNO36rt_1A&oh=00_AfiqivCBkX_uHspz_GDhX_VyAVFJ-Qhbt4dBLvNaaHnPhg&oe=69276467" alt="Vinay Kumar Chelimi" className="w-full h-full object-cover"/>
         </motion.div>
         </motion.div>
         <motion.div className="text-center md:text-left" variants={itemVariants}>
